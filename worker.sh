b38939e8aa2a434ef7e05ee874804cba1603b83d
Erivan Junior <54715081+ErivanJunior275@users.noreply.github.com>
2025-02-25 23:30:01 -0300

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# sudo ufw status
+# sudo ufw allow from 10.172.236.0/24 to any port 2049 proto tcp
+# sudo ufw allow from 10.172.236.0/24 to any port 111 proto tcp
+# sudo ufw allow from 10.172.236.0/24 to any port 111 proto udp
+# sudo ufw allow from 10.172.236.0/24 to any port 20048 proto tcp
+# sudo ufw allow from 10.172.236.0/24 to any port 892 proto tcp
+# sudo ufw allow from 10.172.236.0/24 to any port 875 proto tcp
+# sudo ufw reload
+
+sudo apt install -y nfs-common
+sudo mount -t nfs 10.172.236.100:/var/lib/docker/volumes/sharedfiles/_data /var/lib/docker/volumes/sharedfiles/_data
