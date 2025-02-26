6be653173776b32a8ca953fa52d0f645a9968da5
Erivan Junior <54715081+ErivanJunior275@users.noreply.github.com>
2025-02-25 22:45:01 -0300

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+# Iniciar o Docker Swarm no nó manager
 sudo docker swarm init --advertise-addr=10.172.236.100
+
+# Obter o token do worker e criar o script para ser usado nos nós worker
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+
+# Criar o volume Docker
+sudo docker volume create sharedfiles
+
+# Instalar o servidor NFS
+sudo apt install -y nfs-kernel-server
+
+# Adicionar a configuração do NFS para compartilhar o volume
+echo "/var/lib/docker/volumes/sharedfiles/_data *(rw,sync,subtree_check)" | sudo tee -a /etc/exports
+
+# Atualizar as exportações do NFS
+sudo exportfs -ar
+
+# Reiniciar o serviço NFS depois de modificar (somente se necessário)
+# sudo systemctl restart nfs-kernel-server
+
+# Listar as configurações de exportação
+exportfs -v
