0d302442e2cf022f0a083b5732657d20bbea6a77
Erivan Junior <54715081+ErivanJunior275@users.noreply.github.com>
2025-02-26 00:22:53 -0300

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -13,7 +13,7 @@ sudo apt install -y nfs-common
 # sudo mount -t nfs 10.172.236.100:/var/lib/docker/volumes/sharedfiles/_data /var/lib/docker/volumes/sharedfiles/_data
 
 for i in {1..5}; do
-  sudo mount -t nfs 10.172.236.100:/var/lib/docker/volumes/sharedfiles/_data /var/lib/docker/volumes/sharedfiles/_data && break
+  sudo mount -t nfs 192.168.56.56:/var/lib/docker/volumes/sharedfiles/_data /var/lib/docker/volumes/sharedfiles/_data && break
   echo "Tentando novamente o mount... tentativa $i"
   sleep 15
 done
