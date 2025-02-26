fca1c9c5aff95a38f4c96d748c41541c7a0b6a24
Erivan Junior <54715081+ErivanJunior275@users.noreply.github.com>
2025-02-26 00:50:59 -0300

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -3,14 +3,14 @@
 echo "Aguardando o NFS ser configurado..."
 sleep 20  # Ajuste o tempo conforme necessário
 
-# sudo ufw status
-# sudo ufw allow from 10.172.236.0/24 to any port 2049 proto tcp
-# sudo ufw allow from 10.172.236.0/24 to any port 111 proto tcp
-# sudo ufw allow from 10.172.236.0/24 to any port 111 proto udp
-# sudo ufw allow from 10.172.236.0/24 to any port 20048 proto tcp
-# sudo ufw allow from 10.172.236.0/24 to any port 892 proto tcp
-# sudo ufw allow from 10.172.236.0/24 to any port 875 proto tcp
-# sudo ufw reload
+sudo ufw status
+sudo ufw allow from 10.172.236.0/24 to any port 2049 proto tcp
+sudo ufw allow from 10.172.236.0/24 to any port 111 proto tcp
+sudo ufw allow from 10.172.236.0/24 to any port 111 proto udp
+sudo ufw allow from 10.172.236.0/24 to any port 20048 proto tcp
+sudo ufw allow from 10.172.236.0/24 to any port 892 proto tcp
+sudo ufw allow from 10.172.236.0/24 to any port 875 proto tcp
+sudo ufw reload
 
 sudo apt install -y nfs-common
 
